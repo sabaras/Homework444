0091204f118c18abac2eb78fc5e306f5bebb7aaa
sabmgv <saba.mgvdeladze777@gmail.com>
2022-02-11 22:23:41 +0400

    Add Homework444

---
+++ Homework4444444/Homework4444444/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework4444444
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Saba Mgvdeladze");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Console.ReadLine());
+
+  
+        }
+    }
+}
